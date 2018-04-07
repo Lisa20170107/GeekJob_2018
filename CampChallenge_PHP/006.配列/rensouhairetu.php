e3df28ac2ec82edcac0fee97251cc884f5f1580f
Lisa20170107 <38121865+Lisa20170107@users.noreply.github.com>
2018-04-07 17:04:44 +0900

    Add files via upload

---
+++ CampChallenge_PHP/006.配列/rensouhairetu.php
@@ -0,0 +1,16 @@
+<?php
+
+$date = array(
+        "1"=>"AAA",
+        "hello"=>"world",
+        "soeda"=>"33",
+        "20"=>"20"
+       );
+
+
+echo $date["hello"];
+echo "<br>";
+
+$data['hello'] = 'password';
+
+echo $data['hello'];
