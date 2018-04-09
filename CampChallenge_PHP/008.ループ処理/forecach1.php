de72e5a2619258217a597a758ad9da9cb5936740
Lisa20170107 <38121865+Lisa20170107@users.noreply.github.com>
2018-04-09 14:50:31 +0900

    Add files via upload

---
+++ CampChallenge_PHP/008.ループ処理/forecach1.php
@@ -0,0 +1,18 @@
+<?php
+
+$data1 = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+$date2 = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+$total = 1;
+foreach($data1 as $key) {
+  if ($key == 5){
+    continue;
+  }
+  echo "$key の段です。"."<br>";
+  foreach($date2 as $value){
+    $total = $key*$value;
+    if ($total % 5 == 0){
+      continue;
+    }
+   echo $total."<br>";
+}}
