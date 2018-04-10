2de3e1f0b8d1067918a1e219a7edc5a8233a7b30
Lisa20170107 <38121865+Lisa20170107@users.noreply.github.com>
2018-04-10 10:44:09 +0900

    Add files via upload

--- CampChallenge_PHP/009.関数/hikisu2.php
+++ CampChallenge_PHP/009.関数/hikisu2.php
@@ -1,12 +1,12 @@
 <?php
 
-function hikisu($num1,$num2=5,$num3="false"){
-   if($num3 == "true"){
+function hikisu($num1,$num2=5,$num3=false){
+   if($num3 == true){
      $total = $num1 * $num2;
      echo $total*$total;
-   }else{
-     echo "falseです。";
+   }elseif($num3 == false){
+     echo $num1*$num2;
    }
 }
 
-hikisu(1,2,"A");
+hikisu(1,2,false);
