f15356ef4159f686ec875aab20e68a5fc54850c9
Lisa20170107 <38121865+Lisa20170107@users.noreply.github.com>
2018-04-10 09:49:28 +0900

    Add files via upload

--- CampChallenge_PHP/008.ループ処理/rope1.php
+++ CampChallenge_PHP/008.ループ処理/rope1.php
@@ -1,6 +1,16 @@
 <?php
-
-for($a=2;$a<20;$a++)
+//powの修正
+for($a=2;$a<21;$a++)
 {
   echo pow(8,$a)."<br>";
+}
+
+//powを使わない
+
+$total=1;
+
+for($i=0;$i<20;$i++)
+{
+  $total=$total*8;
+  echo $total ."<br>";
 }
