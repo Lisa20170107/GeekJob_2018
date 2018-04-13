915c69422ac5788c1b520ee5ff65a9c5a77989c2
Lisa20170107 <38121865+Lisa20170107@users.noreply.github.com>
2018-04-13 12:07:25 +0900

    Add files via upload

--- CampChallenge_PHP/011.データ操作/sample01.php
+++ CampChallenge_PHP/011.データ操作/sample01.php
@@ -1,21 +1,14 @@
 <?php
 
 // 名前の情報
-echo $_POST['txtName'];
+echo $_POST['txtName'].'<br>';
 
 // 性別の情報
 
- //もしPOSTに [submit] があれば
-if (isset($_POST["submit"])) {
-//もし男性なら
-if ($_POST["sex"] == "男性") {
-echo "男性";
-}
-//女性なら
-elseif ($_POST["sex"] == "女性") {
-echo "女性";
-}
-}
+echo $_POST['rdoSample'].'<br>';
 
 // 趣味の情報
-echo $_POST['mulText'];
+echo $_POST['mulText'].'<br>';
+
+// 受け取った情報の確認
+var_dump($_POST);
