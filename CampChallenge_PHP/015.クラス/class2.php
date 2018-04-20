78272b0a873c0e35532e9f709aa93f5144b4f8d3
Lisa20170107 <38121865+Lisa20170107@users.noreply.github.com>
2018-04-20 17:36:54 +0900

    Add files via upload

--- CampChallenge_PHP/015.クラス/class2.php
+++ CampChallenge_PHP/015.クラス/class2.php
@@ -33,9 +33,14 @@ $kojin -> setHuman_hyouji();
 // classを継承
 class nomen extends Staff {
     public function crea() {
-      // ２つの変数の中身をクリアするパブリックな関数
+      // ２つの変数の中身をクリアするパブリックな関数（こちらを使うとunset関数により変数自体が破棄されているため、エラーになる
         unset($this->name);
         unset($this->age);
+    }
+    public function crea2() {
+      // ２つの変数の中身をクリアするパブリックな関数のパターン2
+      $this->name = "";
+      $this->age = "";
     }
 }
 
@@ -44,6 +49,6 @@ $human = new nomen();
 // humanインスタンスに情報を入れる
 $human->setHuman('田中敦子',17);
 // humanインスタンスの情報がクリアされる
-$human -> crea('田中敦子',17);
-// 情報がなにもないのでエラーになる
+$human -> crea2();
+// 情報が破棄されたか確認
 $human -> setHuman_hyouji();
