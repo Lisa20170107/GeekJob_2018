a633cb30ed3877f3ea2ff4541a65e717d21d9812
Lisa20170107 <38121865+Lisa20170107@users.noreply.github.com>
2018-05-07 15:49:11 +0900

    Add files via upload

--- CampChallenge_PHP/017.巨大なソースの修正/UserManagementSystem-ver2.0/common/dbaccesUtil.php
+++ CampChallenge_PHP/017.巨大なソースの修正/UserManagementSystem-ver2.0/common/dbaccesUtil.php
@@ -3,7 +3,7 @@
 //DBへの接続を行う。成功ならPDOオブジェクトを、失敗なら中断、メッセージの表示を行う
 function connect2MySQL(){
     try{
-        $pdo = new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8','hayashi','password');
+        $pdo = new PDO('mysql:host=localhost;dbname=Challenge_db;charset=utf8','bm20170107','＊＊＊＊');
         //SQL実行時のエラーをtry-catchで取得できるように設定
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
@@ -16,7 +16,7 @@ function connect2MySQL(){
 function insert_profiles($name, $birthday, $type, $tell, $comment){
     //db接続を確立
     $insert_db = connect2MySQL();
-    
+
     //DBに全項目のある1レコードを登録するSQL
     $insert_sql = "INSERT INTO user_t(name,birthday,tell,type,comment,newDate)"
             . "VALUES(:name,:birthday,:tell,:type,:comment,:newDate)";
@@ -49,15 +49,52 @@ function insert_profiles($name, $birthday, $type, $tell, $comment){
     return null;
 }
 
+//レコードの情報を更新する。失敗した場合はエラー文を返却する。
+function update_profile($name, $birthday, $type, $tell, $comment,$id){
+  //db接続を確立
+  $update_db = connect2MySQL();
+  //DBに全項目のある1レコードを登録するSQL
+  $update_sql = "UPDATE user_t SET name = :name,birthday=:birthday,tell=:tell,type=:type,comment=:comment,newDate=:newDate
+                 WHERE userID= :id";
+
+  //現在時をdatetime型で取得
+  $datetime =new DateTime();
+  $date = $datetime->format('Y-m-d H:i:s');
+
+  //クエリとして用意
+  $update_query = $update_db->prepare($update_sql);
+  $update_query->bindValue(':id',$id);
+
+  //SQL文にセッションから受け取った値＆現在時をバインド
+  $update_query->bindValue(':name',$name);
+  $update_query->bindValue(':birthday',$birthday);
+  $update_query->bindValue(':tell',$tell);
+  $update_query->bindValue(':type',$type);
+  $update_query->bindValue(':comment',$comment);
+  $update_query->bindValue(':newDate',$date);
+
+  //SQLを実行
+  try{
+      $update_query->execute();
+  } catch (PDOException $e) {
+      //接続オブジェクトを初期化することでDB接続を切断
+      $update_db=null;
+      return $e->getMessage();
+  }
+
+  $update_db=null;
+  return null;
+}
+
 function serch_all_profiles(){
     //db接続を確立
     $search_db = connect2MySQL();
-    
+
     $search_sql = "SELECT * FROM user_t";
-    
+
     //クエリとして用意
     $seatch_query = $search_db->prepare($search_sql);
-    
+
     //SQLを実行
     try{
         $seatch_query->execute();
@@ -65,7 +102,7 @@ function serch_all_profiles(){
         $seatch_query=null;
         return $e->getMessage();
     }
-    
+
     //全レコードを連想配列として返却
     return $seatch_query->fetchAll(PDO::FETCH_ASSOC);
 }
@@ -80,28 +117,28 @@ function serch_all_profiles(){
 function serch_profiles($name=null,$year=null,$type=null){
     //db接続を確立
     $search_db = connect2MySQL();
-    
+
     $search_sql = "SELECT * FROM user_t";
     $flag = false;
     if(isset($name)){
-        $search_sql .= " WHERE name like :name";
+        $search_sql .= " WHERE name LIKE :name";
         $flag = true;
     }
     if(isset($year) && $flag = false){
-        $search_sql .= " WHERE birthday like :year";
+        $search_sql .= " AND WHERE birthday LIKE :year";
         $flag = true;
     }else if(isset($year)){
-        $search_sql .= " AND birthday like :year";
+        $search_sql .= " AND birthday LIKE :year";
     }
     if(isset($type) && $flag = false){
-        $search_sql .= " WHERE type = :type";
+        $search_sql .= "AND WHERE type = :type";
     }else if(isset($type)){
         $search_sql .= " AND type = :type";
     }
-    
+
     //クエリとして用意
     $seatch_query = $search_db->prepare($search_sql);
-    
+
     $seatch_query->bindValue(':name','%'.$name.'%');
     $seatch_query->bindValue(':year','%'.$year.'%');
     $seatch_query->bindValue(':type',$type);
@@ -112,24 +149,23 @@ function serch_profiles($name=null,$year=null,$type=null){
         $seatch_query=null;
         return $e->getMessage();
     }
-    
+
     //該当するレコードを連想配列として返却
     return $seatch_query->fetchAll(PDO::FETCH_ASSOC);
 }
 
 
-
 function profile_detail($id){
     //db接続を確立
     $detail_db = connect2MySQL();
-    
+
     $detail_sql = "SELECT * FROM user_t WHERE userID=:id";
-    
+
     //クエリとして用意
     $detail_query = $detail_db->prepare($detail_sql);
-    
+
     $detail_query->bindValue(':id',$id);
-    
+
     //SQLを実行
     try{
         $detail_query->execute();
@@ -137,7 +173,7 @@ function profile_detail($id){
         $detail_query=null;
         return $e->getMessage();
     }
-    
+
     //レコードを連想配列として返却
     return $detail_query->fetchAll(PDO::FETCH_ASSOC);
 }
@@ -145,18 +181,18 @@ function profile_detail($id){
 function delete_profile($id){
     //db接続を確立
     $delete_db = connect2MySQL();
-    
-    $delete_sql = "DELEtE * FROM user_t WHERE userID=:id";
-    
+
+    $delete_sql = "DELETE FROM user_t WHERE userID=:id";
+
     //クエリとして用意
     $delete_query = $delete_db->prepare($delete_sql);
-    
+
     $delete_query->bindValue(':id',$id);
-    
+
     //SQLを実行
     try{
         $delete_query->execute();
-    } catch (PDOException $e) {
+        } catch (PDOException $e) {
         $delete_query=null;
         return $e->getMessage();
     }
--- CampChallenge_PHP/017.巨大なソースの修正/UserManagementSystem-ver2.0/common/defineUtil.php
+++ CampChallenge_PHP/017.巨大なソースの修正/UserManagementSystem-ver2.0/common/defineUtil.php
@@ -1,5 +1,5 @@
 <?php
-const ROOT_URL = 'http://localhost/matome/app';     //indexディレクトリへのURL
+const ROOT_URL = 'http://localhost/UserManagementSystem-ver2.0/app/index.php';     //indexディレクトリへのURL
 const TOP_URI = '/';                                //トップページ
 const INSERT = 'insert.php';                        //登録ページ
 const INSERT_CONFIRM = 'insert_confirm.php';        //登録確認ページ
@@ -9,5 +9,5 @@ const SEARCH_RESULT = 'search_result.php';          //検索結果ページ
 const RESULT_DETAIL = 'result_detail.php';          //検索詳細ページ
 const DELETE = 'delete.php';                        //検索詳細ページ
 const DELETE_RESULT = 'delete_result.php';          //検索詳細ページ
-const UPDATE = 'update.php';                         //検索詳細ページ
+const UPDATE = 'update.php';                        //検索詳細ページ
 const UPDATE_RESULT = 'update_result.php';          //検索詳細ページ
